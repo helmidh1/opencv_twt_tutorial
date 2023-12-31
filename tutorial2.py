bcc226d5bd19e0653166e2a31ce4aca4c68bdd79
Helmi Hernandez <helmidh@hotmail.com>
2023-12-31 14:57:40 -0500

    finished part2 tutorial

---
+++ tutorial2.py
@@ -0,0 +1,11 @@
+import cv2
+import random
+
+img = cv2.imread('assets/logo.jpg', -1)
+
+tag = img[500:700, 600:900]
+img[100:300, 650:950] = tag
+
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
